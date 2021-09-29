fc0085d4e964e8ad2888c5f3c9be9f2111a1fc38
tienchu1108 <88596195+tienchu1108@users.noreply.github.com>
2021-09-29 19:35:04 +0700

    Create jackolanternjuxtaposition.cpp

---
+++ jackolanternjuxtaposition.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int E, T, M;
+    
+    cin >> E >> T >> M;
+    
+    int result = E * T * M;
+    
+    cout << result << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
